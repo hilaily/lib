0fbdb6524f1c5fcf92e2ad2f25c513f9a87fe8a8
laily <i@laily.net>
2025-03-08 14:09:29 +0800

    feat: modify manger

--- llm/manager.go
+++ llm/manager.go
@@ -2,41 +2,71 @@ package llm
 
 import (
 	"fmt"
+	"os"
 
 	"gopkg.in/yaml.v3"
 )
 
-type Manager struct {
-	clients map[string]*Client
+type IManager interface {
+	GetModel(name string) (*Client, bool)
+	GetProvider(name string) (*Client, bool)
 }
 
-func (m *Manager) GetClient(name string) (*Client, bool) {
+type Manager struct {
+	providers map[string]string
+	clients   map[string]*Client
+}
+
+func (m *Manager) GetModel(name string) (*Client, bool) {
 	client, ok := m.clients[name]
 	return client, ok
 }
 
+func (m *Manager) GetProvider(name string) (*Client, bool) {
+	provider, ok := m.providers[name]
+	if !ok {
+		return nil, false
+	}
+	client, ok := m.clients[provider]
+	return client, ok
+}
+
 type LLMConfig struct {
-	Name    string `yaml:"name"`
 	Model   string `yaml:"model"`
 	ApiKey  string `yaml:"apiKey"`
 	BaseUrl string `yaml:"baseUrl"`
 }
 
 type Conf struct {
-	LLM map[string]*LLMConfig `yaml:"llm"`
+	LLM struct {
+		Providers map[string]string     `yaml:"providers"`
+		Models    map[string]*LLMConfig `yaml:"models"`
+	} `yaml:"llm"`
 }
 
 func NewManager(conf string) (*Manager, error) {
-	cfg := &Conf{}
-	err := yaml.Unmarshal([]byte(conf), cfg)
+	confBytes, err := os.ReadFile(conf)
 	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshal llm config: %w, conf: %s", err, conf)
+		return nil, fmt.Errorf("failed to read llm config file: %w, conf: %s", err, conf)
+	}
+	return NewManagerFromData(confBytes)
+}
+
+func NewManagerFromData(confBytes []byte) (*Manager, error) {
+	cfg := &Conf{}
+	err := yaml.Unmarshal(confBytes, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal llm config: %w, conf: %s", err, confBytes)
 	}
 	clients := make(map[string]*Client)
-	for name, cfg := range cfg.LLM {
+	for name, cfg := range cfg.LLM.Models {
 		clients[name] = NewClient(WithAPIKey(cfg.ApiKey), WithBaseURL(cfg.BaseUrl), WithModel(cfg.Model))
 	}
+	if len(clients) == 0 {
+		return nil, fmt.Errorf("no llm clients found, config string: %s, parsed config: %#+v", confBytes, cfg)
+	}
 	return &Manager{
-		clients: clients,
+		providers: cfg.LLM.Providers,
+		clients:   clients,
 	}, nil
 }
