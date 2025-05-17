8b7f44e443e0b92fec61d1abfde557dfe5bb830c
laily <i@laily.net>
2025-05-17 15:05:02 +0800

    feat: add mysql

---
+++ mysql/go.mod
@@ -0,0 +1,22 @@
+module github.com/hilaily/lib/mysql
+
+go 1.24.0
+
+require (
+	github.com/hilaily/lib/configx v0.1.1
+	github.com/sirupsen/logrus v1.9.3
+	gorm.io/driver/mysql v1.5.7
+	gorm.io/gorm v1.26.1
+	gorm.io/plugin/dbresolver v1.6.0
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/hilaily/lib/env v0.3.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ mysql/mysql.go
@@ -0,0 +1,119 @@
+package mysql
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/hilaily/lib/configx"
+	"github.com/sirupsen/logrus"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/plugin/dbresolver"
+)
+
+type IMySQL interface {
+	GetWriteDB() *gorm.DB
+	GetReadDB() *gorm.DB
+}
+
+type DBConfig struct {
+	User     string
+	Pass     string
+	Host     string
+	Port     int
+	Database string
+}
+
+func New(conf configx.IConfig) (*_mysql, error) {
+	var writeConf *DBConfig
+	ok, err := conf.Get("mysql_write", writeConf)
+	if !ok {
+		return nil, fmt.Errorf("mysql write config not found")
+	}
+	if err != nil {
+		return nil, fmt.Errorf("get mysql write config failed: %w", err)
+	}
+	writeDB, writeDSN, err := connectDB(writeConf.User, writeConf.Pass, writeConf.Host, writeConf.Port, writeConf.Database)
+	if err != nil {
+		return nil, fmt.Errorf("write db connect failed:%w", err)
+	}
+	res := &_mysql{
+		writeDB: writeDB,
+	}
+
+	var readConf *DBConfig
+	hasRead, err := conf.Get("mysql_read", readConf)
+	if hasRead {
+		if err != nil {
+			return nil, fmt.Errorf("get mysql read config failed: %w", err)
+		}
+	} else {
+		logrus.Warn("mysql read config not found")
+	}
+
+	if readConf != nil {
+		// 读库配置
+		readDB, readDSN, err := connectDB(readConf.User, readConf.Pass, readConf.Host, readConf.Port, readConf.Database)
+		if err != nil {
+			return nil, fmt.Errorf("read db connect failed:%w", err)
+		}
+		err = writeDB.Use(dbresolver.Register(dbresolver.Config{
+			Sources:  []gorm.Dialector{mysql.Open(writeDSN)}, // 写操作使用的数据源
+			Replicas: []gorm.Dialector{mysql.Open(readDSN)},  // 读操作使用的数据源
+			Policy:   dbresolver.RandomPolicy{},              // 负载均衡策略
+		}).SetConnMaxIdleTime(time.Hour).
+			SetConnMaxLifetime(24 * time.Hour).
+			SetMaxIdleConns(100).
+			SetMaxOpenConns(200))
+		if err != nil {
+			panic("can not config read write split: " + err.Error())
+		}
+		res.readDB = readDB
+	}
+
+	logrus.Info("database connect success")
+	return res, nil
+}
+
+func connectDB(user, pass, host string, port int, dbName string) (*gorm.DB, string, error) {
+	// 主库（写库）配置
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local&timeout=10s", user, pass, host, port, dbName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err == nil {
+		return db, dsn, nil
+	}
+
+	if !strings.Contains(err.Error(), "Unknown database") {
+		return nil, "", fmt.Errorf("connect to database failed: %w", err)
+	}
+
+	tmpDSN := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local&timeout=10s", user, pass, host, port, "mysql")
+
+	db, err = gorm.Open(mysql.Open(tmpDSN), &gorm.Config{})
+	if err != nil {
+		return nil, "", fmt.Errorf("connect to database failed: %w", err)
+	}
+
+	// 创建数据库
+	err = db.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s DEFAULT CHARACTER SET utf8mb4", dbName)).Error
+	if err != nil {
+		return nil, "", fmt.Errorf("create database failed: %w", err)
+	}
+
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	return db, dsn, err
+}
+
+type _mysql struct {
+	writeDB *gorm.DB
+	readDB  *gorm.DB
+}
+
+func (m *_mysql) GetWriteDB() *gorm.DB {
+	return m.writeDB
+}
+
+func (m *_mysql) GetReadDB() *gorm.DB {
+	return m.readDB
+}
